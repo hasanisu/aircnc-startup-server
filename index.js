605c4b34dd418d295172d136c063dabf4119614b
hasanisu <hasanjimam@gmail.com>
2023-08-03 18:30:27 +0900

    added stripe

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const nodemailer = require("nodemailer");
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 
 const app = express()
@@ -11,6 +13,52 @@ const port = process.env.PORT || 7000
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+//JWT
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unathorized access'})
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'Foriddend access'})
+    }
+    console.log(decoded)
+    req.decoded = decoded
+    next()
+  })
+}
+
+//Send Email
+const sendMail = (emailData, email) =>{
+  //...https://miracleio.me/snippets/use-gmail-with-nodemailer 
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADD,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from:process.env.EMAIL_ADD, 
+    to: email,
+    subject: emailData?.subject,
+    html: `<p>${emailData?.message}</p>`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+   console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+}
 
 // Database Connection
 // const uri = "mongodb://localhost:27017"
@@ -91,9 +139,50 @@ async function run() {
       const bookingData = req.body;
       const result = await bookingsCollection.insertOne(bookingData);
       console.log(result)
+      sendMail(
+        {
+          subject:'Booking Successful', 
+          message:`Booking Id: ${result?.insertedId}`
+        }, bookingData?.guestEmail)
+
       res.send(result)
 
     })
+
+
+    //Create Payment intent
+    app.post('/create-payment-intent', async (req, res)=>{
+
+      const price = req.body.price;
+      const amount = parseFloat(price * 100)
+
+      try {
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'jpy',
+          payment_method_types: ['card'],
+        })
+
+        res.send({clientSecret: paymentIntent.client_secret})
+        
+      } catch (error) {
+        console.log(error)
+      }
+
+
+
+    })
+
+
+    // Delete Booking
+    app.delete('/booking/:id', async (req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const deleteBooking = await bookingsCollection.deleteOne(filter)
+      res.send(deleteBooking);
+    }) 
+
 
 
     //get Home
